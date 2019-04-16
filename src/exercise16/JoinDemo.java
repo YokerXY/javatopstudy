0984c07d2af1b7b6e81c6443eedcf3b94a173766
ASUS <2414704205@qq.com>
2019-04-17 06:18:38 +0800

    Join的练习

---
+++ src/exercise16/JoinDemo.java
@@ -0,0 +1,33 @@
+package exercise16;
+
+public class JoinDemo extends Thread {
+    public void run(){
+        for (int i = 10;i>-1;i--){
+            System.out.println(i);
+            if (i <= 0 ){
+            }
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("开始倒数");
+        Thread thread = new Thread();
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        JoinDemo joinDemo = new JoinDemo();
+        joinDemo.start();
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
